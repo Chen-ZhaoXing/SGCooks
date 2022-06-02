650f85104e1284cb9acdae9c3fda2ff35e3007d4
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-06-02 20:30:19 +0800

    SGCooks Spring set up v1

---
+++ Back-End/Heap/src/main/java/com/Heap/SGCooks/SGCooksController.java
@@ -0,0 +1,24 @@
+package com.Heap.SGCooks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path ="SGCooks")
+public class SGCooksController {
+    private final SGCooksService sgCooksService;
+
+    @Autowired
+    public SGCooksController(SGCooksService sgCooksService){
+        this.sgCooksService = sgCooksService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return sgCooksService.getUsers();
+    }
+}
