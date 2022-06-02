21993a41f16ef1b0639617aa505d6fac5681e3f3
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-06-02 20:51:08 +0800

    Added userConfig file and added 1 dummy data

--- Back-End/Heap/src/main/java/com/Heap/SGCooks/SGCooksController.java
+++ Back-End/Heap/src/main/java/com/Heap/SGCooks/SGCooksController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path ="SGCooks")
+@RequestMapping(path ="api/v1/SGCooks")
 public class SGCooksController {
     private final SGCooksService sgCooksService;
 
