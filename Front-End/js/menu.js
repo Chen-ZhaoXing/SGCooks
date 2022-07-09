1cf9b998acdd1b2e2a3a3e6e02e83c03a1fed299
Novia Antony <noviaantony2001@gmail.com>
2022-07-09 23:32:39 +0800

    updated menu.html

--- Front-End/js/menu.js
+++ Front-End/js/menu.js
@@ -1,13 +1,15 @@
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
+const ingredientsAccordian = document.querySelector('.ingredients-item');
+
 const container = document.querySelector('.container');
 let searchQuery = '';
-const apiKey = "e3ffe551a5d94a358a460e7946136869";
+const apiKey = "f4a4c702a03b4236807d9a23325cf6f5";
 
 
 // generating all recipes when u press menu.html
 
-// allRecipes();
+allRecipes();
 
 async function allRecipes() {
 
@@ -44,9 +46,6 @@ async function allRecipes() {
               <div class="modal-body" id="recipe-details"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              </div>
             </div>
           </div>
         </div>
@@ -58,15 +57,6 @@ async function allRecipes() {
   searchResultDiv.innerHTML = generatedHTML;
 
 }
-
-
-
-
-
-
-
-
-
 
 
 // generating the recipes based on search
@@ -111,9 +101,6 @@ async function searchRecipes() {
               <div class="modal-body" id="recipe-details"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              </div>
             </div>
           </div>
         </div>
@@ -125,7 +112,7 @@ async function searchRecipes() {
   searchResultDiv.innerHTML = generatedHTML;
 }
 
-// generating modal with time, serving
+// generating modal with time, serving 
 
 document.addEventListener('click', function(e){
   if (e.target && e.target.classList.contains('btn-viewrecipe')){
@@ -134,31 +121,84 @@ document.addEventListener('click', function(e){
   }
 })
 
-// generating modal with instructions
+const encodeBase64 = (data) => {
+  return Buffer.from(data).toString('base64');
+}
 
-document.addEventListener('click', function(e){
-  if (e.target && e.target.classList.contains('btn-viewrecipe')){
-    e.preventDefault();
-    addIngredients(e);
-  }
-})
+function toBase64(dataArr){
+  return btoa(dataArr.reduce((data, val)=> {
+    return data + String.fromCharCode(val);
+  }, ''));
+}
+
+const blobToBase64 = blob => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(blob);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = error => reject(error);
+});
+
+const convertBlobToBase64 = async (blob) => {
+  return await blobToBase64(blob);
+}
 
 async function getMealRecipe(e){
       let recipe= e.target.parentElement.parentElement;
       console.log(recipe)
       let recipeId = e.target.parentElement.parentElement.querySelector('.recipe-id').innerText; // !!! retrieves the value from the search bar
       console.log(recipeId);
-      const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
-      console.log('success');
-      const response = await fetch(url);
-      const data = await response.json();
+      const url1 = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
+      const url2 = `https://api.spoonacular.com/recipes/${recipeId}/ingredientWidget.png&apiKey=${apiKey}`;
+
+      const response1 = await fetch(url1);
+      const data = await response1.json();
+      console.log('success1');
+
+      const response2 = await fetch(
+        `https://api.spoonacular.com/recipes/${recipeId}/ingredientWidget.png?measure=metric&apiKey=${apiKey}`,
+        {
+          method: 'GET',
+          headers: {
+            'Accept': 'image/png',
+            'Content-Type': 'image/png'
+          }
+        }
+      );
+      if (!response2.ok) {
+        throw new Error(`HTTP error! status: ${response2.status}`);
+      }
+
+
+      const data2 = await response2.blob();
+      console.log('success2');
+      const temp1 =  await convertBlobToBase64(data2);
+
+      //
+
+      const response3 = await fetch(
+        `https://api.spoonacular.com/recipes/${recipeId}/equipmentWidget.png?measure=metric&apiKey=${apiKey}`,
+        {
+          method: 'GET',
+          headers: {
+            'Accept': 'image/png',
+            'Content-Type': 'image/png'
+          }
+        }
+      );
+      if (!response3.ok) {
+        throw new Error(`HTTP error! status: ${response2.status}`);
+      }
+
+      const data3 = await response3.blob();
+      console.log('success2');
+      const temp2 =  await convertBlobToBase64(data3);
+
 
       recipeModal = document.getElementById('recipe-details');
-      console.log(recipeModal)
+
 
       let generatedHTML = "";
 
-      
         generatedHTML += ` 
           <!-- Modal -->
                   <img src="${data.image}" alt="..." style = "height: 300px; width: 400px; object-fit: contain;">
@@ -168,58 +208,55 @@ async function getMealRecipe(e){
                           <p class = "recipe-category">${data.cuisines}</p>
                           <p class = "recipe-category">Preparation Time: ${data.readyInMinutes}</p>
                           <p class = "recipe-category">Servings: ${data.servings}</p>
-                          <p class = "recipe-category">Price: ${data.pricePerServing / 10}</p>
+                          <p class = "recipe-category">Price: ${data.pricePerServing / 100}</p>
                           <div class = "recipe-instruct">
-                            <h3>Instructions:</h3> 
+                          <div class="accordion accordion-flush" id="accordionFlushExample">
+                          <div class="accordion-item instructions-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                Instructions
+                              </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                              <div class="accordion-body">${data.instructions}</div>
+                            </div>
+                          </div>
+                          <div class="accordion-item ingredients-item">
+                            <h2 class="accordion-header" id="flush-headingTwo">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                Ingredients
+                              </button>
+                            </h2>
+                            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                              <div class="accordion-body">
+                                <img src="${temp1}" alt="">
+                              </div>
+                            </div>
+                          </div>
+                          <div class="accordion-item equipment-item">
+                            <h2 class="accordion-header" id="flush-headingThree">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                Equipment
+                              </button>
+                            </h2>
+                            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                              <div class="accordion-body">
+                                <img src="${temp2}" alt="">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                           </div>
                           <div class = "recipe-link">
                             <a href = "https://www.youtube.com/watch?v=GA-fCgJykEw" target = "_blank">Watch Video</a>
                           </div> 
                     </div>
       `
-      
-
+    
       recipeModal.innerHTML = generatedHTML;
 }
 
 
-
-
-async function addIngredients(e){
-      let recipe= e.target.parentElement.parentElement;
-      console.log(recipe)
-      let recipeId = e.target.parentElement.parentElement.querySelector('.recipe-id').innerText; // !!! retrieves the value from the search bar
-      console.log(recipeId);
-      const url = `https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions&apiKey=${apiKey}`;
-      console.log('success');
-      const response = await fetch(url);
-      const data = await response.json();
-
-      recipeModal = document.getElementById('recipe-instruct');
-      console.log(recipeModal)
-
-      let generatedHTML = "";
-
-      
-
-      var len = data.length;
-
-      for (var i = 0; i < len ; i++) {
-
-        generatedHTML += ` 
-        <p>${data.steps.step[i]}</p>
-      `
-
-      }
-
-
-        
-      
-
-      recipeModal.innerHTML = generatedHTML;
-}
-
-// generating modal with ingredients
 
 
 
