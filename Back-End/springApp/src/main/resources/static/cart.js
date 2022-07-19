aa470cc4d5b9c7f0318c9235850429d0490c02d7
chongyuansheng <chongyuansheng@gmail.com>
2022-07-19 16:03:52 +0800

    Updated cart and order ajax

--- Back-End/springApp/src/main/resources/static/cart.js
+++ Back-End/springApp/src/main/resources/static/cart.js
@@ -1,5 +1,38 @@
 getCartItems();
 
+document.addEventListener("click", function(e){
+    if (e.target && e.target.classList.contains('btn-delete')){
+        e.preventDefault();
+        removeCartItem(e);
+    } else if (e.target && e.target.classList.contains('btn-uparrow')) {
+        e.preventDefault();
+        changeItemQuantity();
+    }
+
+})
+
+async function removeCartItem(e) {
+    let cartItemId = e.target.parentElement.parentElement.querySelector('.cart-id').innerText;
+    let email = localStorage.getItem("email");
+
+    $.ajax({
+        type : "DELETE",
+        contentType : "application/json",
+        url : "api/v1/sgcooks/cart/removeItem/" + cartItemId + "?email=" + email,
+        //data : JSON.stringify(formData),
+        dataType : 'text',
+        success : function(result) {
+
+
+            console.log(result);
+        },
+        error : function(e) {
+            alert("Error!")
+            console.log("ERROR: ", e);
+        }
+    });
+}
+
 async function getCartItems() {
     console.log("test");
     let email = "novia@gmail.com"; //temp variable, has to be dynamic
@@ -16,7 +49,6 @@ async function getCartItems() {
 
             let generatedHTML = "";
 
-
             for (var i = 0; i < size; i++) {
 
 
@@ -31,5 +63,29 @@ async function getCartItems() {
             console.log("ERROR: ", e);
         }
     });
+
+
 }
 
+async function changeItemQuantity(e) {
+    let email = localStorage.getItem("email");
+    let cartItemId = e.target.parentElement.parentElement.querySelector('.cart-id').innerText;
+    let quantity = e.target.parentElement.parentElement.querySelector('.quantity').innerText;
+
+    $.ajax({
+        type : "PUT",
+        contentType : "application/json",
+        url : "api/v1/sgcooks/cart/changeCartItemQuantity?email=" + email + "&cartItemId=" + cartItemId + "&quantity=" + quantity,
+        //data : JSON.stringify(formData),
+        dataType : 'text',
+        success : function(result) {
+
+
+            console.log(result);
+        },
+        error : function(e) {
+            alert("Error!")
+            console.log("ERROR: ", e);
+        }
+    });
+}
--- Back-End/springApp/src/main/resources/static/order.js
+++ Back-End/springApp/src/main/resources/static/order.js
@@ -1,83 +1,62 @@
-$(document).ready(
-    function() {
+getOrders();
 
-        // Place order (NOTE Change the #placeorder accordingly
-        $("#placeOrder").submit(function(event) {
-            // Prevent the form from submitting via the browser.
-            event.preventDefault();
-            orderPost();
-        });
+async function orderPost(e) {
 
-        function orderPost() {
-
-            // PREPARE FORM DATA
-            // var formData = {
-            //     email : $("#email").val() // i think this one need to extract or stored somehwere
-            // };
-            let email = $("#email").val();
-            console.log(email);
-            // DO POST
-            $.ajax({
-                type : "POST",
-                contentType : "application/json",
-                url : "api/v1/sgcooks/order/add?email=" + email,
-                //data : JSON.stringify(formData),
-                dataType : 'json',
-                success : function(result) {
-                    // if (result.status == "success") {
-                    //     $("#postResultDiv").html(
-                    //         "" + result.data.bookName
-                    //         + "Post Successfully! <br>"
-                    //         + "---> Congrats !!" + "</p>");
-                    // } else {
-                    //     $("#postResultDiv").html("<strong>Error</strong>");
-                    // }
-                    $("#postResultDiv").html(
-                        "" + result.message
-                        + "Post Successfully! <br>"
-                        + "---> Congrats !!" + "</p>");
-                    console.log(result);
-                },
-                error : function(e) {
-                    alert("Error!")
-                    console.log("ERROR: ", e);
-                }
-            });
-
+    let email = $("#email").val();
+    console.log(email);
+    // DO POST
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "api/v1/sgcooks/order/add?email=" + email,
+        //data : JSON.stringify(formData),
+        dataType: 'json',
+        success: function (result) {
+            $("#postResultDiv").html(
+                "" + result.message
+                + "Post Successfully! <br>"
+                + "---> Congrats !!" + "</p>");
+            console.log(result);
+        },
+        error: function (e) {
+            alert("Error!")
+            console.log("ERROR: ", e);
         }
+    });
 
-        $("#getOrder").click(function(event) {
-            console.log("tests");
-            // Prevent the form from submitting via the browser.
-            event.preventDefault();
-            getOrders();
-        });
+}
 
-        function getOrders() {
-            let email = "novia@gmail.com"; //temp variable, has to be dynamic
-            $.ajax({
-                type : "GET",
-                url : "api/v1/sgcooks/order/?email="+ email,
-                success : function(result) {
-                    console.log(result);
 
-                    // if (result.status == "success") {
-                    $('#getOrderDiv ul').empty();
-                    $.each(result,
-                        function(i,order) {
-                            var orderValues = order.id + "<br>";
-                            $('#getOrderDiv .list-group').append(
-                                orderValues )
-                        });
-                    console.log("Success: ", result);
+async function getOrders() {
+    let email = "novia@gmail.com"; //temp variable, has to be dynamic
+    $.ajax({
+        type: "GET",
+        url: "api/v1/sgcooks/order/?email=" + email,
+        success: function (result) {
+            console.log(result);
 
-                },
-                error : function(e) {
-                    $("#getResultDiv").html("<strong>Error</strong>");
-                    console.log("ERROR: ", e);
-                }
-            });
+            let orderItems = result;
 
+            let size = orderItems.length;
+
+            let generatedHTML = "";
+
+            for (var i = 0; i < size; i++) {
+
+
+                generatedHTML += `
+    
+    `;
+            } // keys for orders: productid , email ; store both cartId and productId
+            searchResultDiv.innerHTML = generatedHTML;
+
+        },
+        error: function (e) {
+            $("#getResultDiv").html("<strong>Error</strong>");
+            console.log("ERROR: ", e);
         }
-    })
+    });
+
+
+}
 
