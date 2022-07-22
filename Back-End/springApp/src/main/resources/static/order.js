0a77cba41104cf281b23b268ab2b9e0dbfa7185e
Chen Zhao Xing <chenzhaoxing.98@gmail.com>
2022-07-22 12:00:59 +0800

    Linked up "order" Ajax with HTMl elements (placing orders, retrieving orders)

--- Back-End/springApp/src/main/resources/static/order.js
+++ Back-End/springApp/src/main/resources/static/order.js
@@ -1,8 +1,18 @@
 getOrders();
 
+const orderBody = document.querySelector('.order-body');
+
+document.addEventListener("click", function(e){
+    if (e.target && e.target.classList.contains('btn-checkout')){
+        e.preventDefault();
+        orderPost(e);
+    }
+
+})
+
 async function orderPost(e) {
 
-    let email = $("#email").val();
+    let email = "novia@gmail.com"; //temp variable, has to be dynamic
     console.log(email);
     // DO POST
     $.ajax({
@@ -12,10 +22,6 @@ async function orderPost(e) {
         //data : JSON.stringify(formData),
         dataType: 'json',
         success: function (result) {
-            $("#postResultDiv").html(
-                "" + result.message
-                + "Post Successfully! <br>"
-                + "---> Congrats !!" + "</p>");
             console.log(result);
         },
         error: function (e) {
@@ -32,23 +38,50 @@ async function getOrders() {
     $.ajax({
         type: "GET",
         url: "api/v1/sgcooks/order/?email=" + email,
-        success: function (result) {
+        success: async function (result) {
             console.log(result);
 
-            let orderItems = result;
+            let order = result;
 
-            let size = orderItems.length;
+            let size = order.length;
 
             let generatedHTML = "";
 
             for (var i = 0; i < size; i++) {
+                let recipeId = order[i].orderItems[i].productId;
+                const url1 = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`;
 
+                const response1 = await fetch(url1);
+                const data = await response1.json();
+                let price = Math.floor((Math.random() * 20) + 10);
+
+                console.log(recipeId);
+                console.log(data.title);
 
                 generatedHTML += `
+                     <div class="d-flex flex-row justify-content-between align-items-center p-2 bg-white mt-4 px-3 rounded" style="box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;">
+                        <div class="mr-1"><img class="rounded" src="assets/img/chicken_soup.jpg" width="70"></div>
+                        <div class="d-flex flex-column align-items-center product-details"><span class="font-weight-bold">${data.title}</span>
+                            <div class="d-flex flex-row product-desc">
+                                <div class="color" style="margin-right: 10px;"><span class="text-grey">Product ID:</span><span class="font-weight-bold">1234</span></div>
+                                <div class="color"><span class="text-grey">Cart ID:</span><span class="font-weight-bold">1234</span></div>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-row align-items-center qty">
+                                <input id="quantity" type="number" value ="1" class="form-control quantity-input" style="margin-right: 20px;">
+                        </div>
+                        <div>
+                            <h5 class="text-grey">$20.00</h5>
+                        </div>
+                        
+                        <div>
+                          <h5 class="text-grey">20/07/2022</h5>
+                      </div>
+                    </div>
     
-    `;
+                `;
             } // keys for orders: productid , email ; store both cartId and productId
-            searchResultDiv.innerHTML = generatedHTML;
+            orderBody.innerHTML = generatedHTML;
 
         },
         error: function (e) {
